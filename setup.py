4af4ef056653f16ab49afd8887defb274f112d38
ThresholdFlux <dans.landers@gmail.com>
2018-02-04 23:15:28 -0500

    Added Neg eigval plot in nb. Updated setup file w/ github url, added nullspace rank funcs from scipy-cookbook.

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(
     name='sunypi_physics',
     version='0',
-    packages=['models.kronig_penney', 'utilities.find_roots', 'utilities.rank_nullspace', 'constants'],
+    packages=['models.kronig_penney.kronig_penney', 'utilities.find_roots', 'utilities.rank_nullspace', 'constants'],
     install_requires=['numpy', 'scipy', 'matplotlib'],
-    url='',
+    url='https://github.com/ThresholdFlux/sunypi_physics',
     license='',
     author='Daniel Landers',
     author_email='dans.landers@gmail.com',
